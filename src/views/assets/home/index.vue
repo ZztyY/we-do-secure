8bd72085ad5601d19a510e0bef40e3d1bc7b786a
Tianyi Zheng <1908575756@qq.com>
2022-08-08 19:41:57 -0400

    update views

--- src/views/assets/home/index.vue
+++ src/views/assets/home/index.vue
@@ -1,21 +1,21 @@
 <template>
-    <el-row class="home" :gutter="20">
-        <el-col :span="8" style="margin-top: 20px">
-            <el-card shadow="hover">
-                <div class="user">
-                    <img class="userImg" :src="userImg">
-                    <div class="userInfo">
-                        <p class="name">Admin</p>
-                        <p class="role">SuperManager</p>
+    <div>
+        <el-row :gutter="20" v-for="(row, index) in sliceList(homeList, 2)" :key="index">
+            <el-col :span="8" style="margin-top: 20px;" v-for="(item, i) in row" :key="i">
+                <el-card shadow="hover" :class="'home'+ i">
+                    <div style="display:flex; justify-content:space-between;">
+                        <i class="el-icon-s-home"></i>
+                        <span> ${{ item.PUR_VAL }} </span>
                     </div>
-                </div>
-                <div class="login-info">
-                    <p>Last login time:<span>2021-7-19</span></p>
-                    <p>Last login location:<span>NY</span></p>
-                </div>
-            </el-card>
-        </el-col>
-    </el-row>
+                    <el-divider></el-divider>
+                    <div>
+                        <p>Area: {{ item.hAREA}}</p>
+                        <p>Home Type: {{ item.hTYPE}}</p>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
@@ -24,10 +24,86 @@ export default {
     name: 'home-view',
     data () {
         return {
-            userImg: require('../../../assets/logo.png')
         }
     },
     mounted() {
+    },
+    computed: {
+        homeList(){
+            return [
+            {
+                hID: 1,
+                PUR_DATE: '2022/08/14',
+                PUR_VAL: 1000000.00,
+                hAREA: 300,
+                hTYPE: 'S',
+                hAFN: 0,
+                hHSS: 0,
+                hSP: null,
+                hBM: 1
+            },
+            {
+                hID: 2,
+                PUR_DATE: '2022/08/14',
+                PUR_VAL: 1000000.00,
+                hAREA: 300,
+                hTYPE: 'S',
+                hAFN: 0,
+                hHSS: 0,
+                hSP: null,
+                hBM: 1
+            },
+            {
+                hID: 3,
+                PUR_DATE: '2022/08/14',
+                PUR_VAL: 1000000.00,
+                hAREA: 300,
+                hTYPE: 'S',
+                hAFN: 0,
+                hHSS: 0,
+                hSP: null,
+                hBM: 1
+            },
+            {
+                hID: 4,
+                PUR_DATE: '2022/08/14',
+                PUR_VAL: 1000000.00,
+                hAREA: 300,
+                hTYPE: 'S',
+                hAFN: 0,
+                hHSS: 0,
+                hSP: null,
+                hBM: 1
+            },
+            {
+                hID: 5,
+                PUR_DATE: '2022/08/14',
+                PUR_VAL: 1000000.00,
+                hAREA: 300,
+                hTYPE: 'S',
+                hAFN: 0,
+                hHSS: 0,
+                hSP: null,
+                hBM: 1
+            }
+            ]
+        }
+    },
+    methods: {
+        sliceList(data, count) {
+            if (data != undefined) {
+                let index = 0;
+                let tempArr = [];
+                for (let i = 0; i< data.length; i++) {
+                    index = parseInt(i / count);
+                    if (tempArr.length <= index) {
+                        tempArr.push([]);
+                    }
+                    tempArr[index].push(data[i]);
+                }
+                return tempArr
+            }
+        }
     }
 }
 </script>
--- src/views/policy/autoPolicies/index.vue
+++ src/views/policy/autoPolicies/index.vue
@@ -1,21 +1,22 @@
 <template>
-    <el-row class="autoPolicies" :gutter="20">
-        <el-col :span="8" style="margin-top: 20px">
-            <el-card shadow="hover">
-                <div class="user">
-                    <img class="userImg" :src="userImg">
-                    <div class="userInfo">
-                        <p class="name">Admin</p>
-                        <p class="role">SuperManager</p>
+    <div>
+        <el-row :gutter="20" v-for="(row, index) in sliceList(homePoliciesList, 2)" :key="index">
+            <el-col :span="8" style="margin-top: 20px;" v-for="(item, i) in row" :key="i">
+                <el-card shadow="hover" :class="'homePolicies'+ i">
+                    <div style="display:flex; justify-content:space-between;">
+                        <i class="el-icon-document"></i>
+                        <span> #{{ item.pID }} </span>
                     </div>
-                </div>
-                <div class="login-info">
-                    <p>Last login time:<span>2021-7-19</span></p>
-                    <p>Last login location:<span>NY</span></p>
-                </div>
-            </el-card>
-        </el-col>
-    </el-row>
+                    <el-divider></el-divider>
+                    <div>
+                        <p>Policy Amount: ${{ item.pAMOUNT}}</p>
+                        <p v-if="item.pSTATUS === 'C'" style="color: green">CURRENT</p>
+                        <p v-else style="color: red">EXPIRED</p>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
@@ -24,18 +25,79 @@ export default {
     name: 'auto-policies-view',
     data () {
         return {
-            userImg: require('../../../assets/logo.png')
         }
     },
     mounted() {
+    },
+    computed: {
+        homePoliciesList(){
+            return [
+            {
+                pID: 16348123,
+                START_DATE: '2010/07/22',
+                END_DATE: '2020/7/22',
+                pAMOUNT: 4000,
+                pSTATUS: 'P',
+                pTYPE: 'H',
+                cID: 1
+            },
+            {
+                pID: 16349999,
+                START_DATE: '2010/07/22',
+                END_DATE: '2024/7/22',
+                pAMOUNT: 6000,
+                pSTATUS: 'C',
+                pTYPE: 'H',
+                cID: 1
+            },
+            {
+                pID: 18948123,
+                START_DATE: '2010/07/22',
+                END_DATE: '2020/7/22',
+                pAMOUNT: 4000,
+                pSTATUS: 'P',
+                pTYPE: 'H',
+                cID: 1
+            },
+            {
+                pID: 16309123,
+                START_DATE: '2010/07/22',
+                END_DATE: '2020/7/22',
+                pAMOUNT: 4000,
+                pSTATUS: 'P',
+                pTYPE: 'H',
+                cID: 1
+            },
+            {
+                pID: 16301723,
+                START_DATE: '2010/07/22',
+                END_DATE: '2020/7/22',
+                pAMOUNT: 4000,
+                pSTATUS: 'P',
+                pTYPE: 'H',
+                cID: 1
+            }
+            ]
+        }
+    },
+    methods: {
+        sliceList(data, count) {
+            if (data != undefined) {
+                let index = 0;
+                let tempArr = [];
+                for (let i = 0; i< data.length; i++) {
+                    index = parseInt(i / count);
+                    if (tempArr.length <= index) {
+                        tempArr.push([]);
+                    }
+                    tempArr[index].push(data[i]);
+                }
+                return tempArr
+            }
+        }
     }
 }
 </script>
 
 <style lang="less" scoped>
-    .userImg{
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-    }
 </style>
